da18cc9c7f23f8f739b1dcc44a4e14f06efe4abd
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2022-04-27 22:02:59 +0200

    Millores durant el covid

--- Scripts/Canal_FloatFloat.cs
+++ Scripts/Canal_FloatFloat.cs
@@ -1,4 +1,3 @@
-
 [UnityEngine.CreateAssetMenu(menuName = "Xido Studio/Esdeveniment/FloatFloat")]
 public class Canal_FloatFloat : Esdeveniment.Canal<float, float> { }
 
---
+++ Scripts/Canal_FloatFloatFloat.cs
@@ -0,0 +1,2 @@
+[UnityEngine.CreateAssetMenu(menuName = "Xido Studio/Esdeveniment/FloatFloatFloat")]
+public class Canal_FloatFloatFloat : Esdeveniment.Canal<float, float, float> { }
